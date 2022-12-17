e6ffaf1100cefb50353bc3ba163ce33e829b7956
seth venhaus <venhaus.seth@gmail.com>
2022-12-17 10:28:48 -0800

    made button to move between edit and display page

--- Controllers/GiftExchangeController.cs
+++ Controllers/GiftExchangeController.cs
@@ -60,7 +60,7 @@ public class GiftExchangeController : Controller
     [HttpGet("giftexchanges/{GiftExchangeId}")]
     public IActionResult OneGiftExchange(int GiftExchangeId)
     {
-        System.Console.WriteLine("test");
+        // System.Console.WriteLine("test");
         MyViewModel MyModels = new MyViewModel
         {
             GiftExchange = _context.GiftExchanges.Include(ge=>ge.MemberList)
@@ -69,5 +69,38 @@ public class GiftExchangeController : Controller
         };
 
         return View(MyModels);
+    }
+
+    //******************************EDIT GIFT EXCHANGE***********************************************
+    [SessionCheck]
+    [HttpGet("giftexchanges/{GiftExchangeId}/edit")]
+    public IActionResult EditGiftExchange(int GiftExchangeId)
+    {
+        GiftExchange? GEInDb = _context.GiftExchanges.SingleOrDefault(ge=>ge.GiftExchangeId == GiftExchangeId);
+        return View(GEInDb);
+    }
+
+    //******************************UPDATE GIFT EXCHANGE***********************************************
+    [SessionCheck]
+    [HttpPost("giftexchanges/{GiftExchangeId}/update")]
+    public IActionResult UpdateGiftExchange(GiftExchange GEToUpdate, int GiftExchangeId)
+    {
+        System.Console.WriteLine(GEToUpdate.Name);
+        if (ModelState.IsValid)
+        {
+            System.Console.WriteLine("test - after IsValid");
+            GiftExchange? OldGE = _context.GiftExchanges.FirstOrDefault(ge=>ge.GiftExchangeId == GiftExchangeId);
+
+            OldGE.Name = GEToUpdate.Name;
+            OldGE.GroupCode = GEToUpdate.GroupCode;
+            OldGE.Budget = GEToUpdate.Budget;
+            OldGE.ExchangeDate = GEToUpdate.ExchangeDate;
+            OldGE.Details = GEToUpdate.Details;
+            OldGE.UpdatedAt = DateTime.Now;
+            _context.SaveChanges();
+
+            return RedirectToAction("OneGiftExchange", GiftExchangeId);
+        }
+        return View("EditGiftExchange", GiftExchangeId);
     }
 }

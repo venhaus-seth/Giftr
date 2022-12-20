fcf57383f764b1ba541e22ed86cfee01e72bed27
seth venhaus <venhaus.seth@gmail.com>
2022-12-20 11:08:15 -0800

    added edit button

--- Controllers/ItemController.cs
+++ Controllers/ItemController.cs
@@ -18,8 +18,9 @@ public class ItemController : Controller
         _context = context;
     }
 
+//******************************SHOW A WISHLIST***********************************************
     [SessionCheck]
-    [HttpGet("Items/{UserId}")]
+    [HttpGet("items/{UserId}")]
     public IActionResult OneWishList(int UserId)
     {
         MyViewModel MyModels = new MyViewModel
@@ -29,5 +30,66 @@ public class ItemController : Controller
                                 .FirstOrDefault(u=>u.UserId == UserId)
         };
     return View(MyModels);
+    }
+
+//******************************ADD ITEM FORM***********************************************
+    [SessionCheck]
+    [HttpGet("items/new")]
+    public IActionResult AddItem()
+    {
+        MyViewModel MyModels = new MyViewModel
+        {
+            User = _context.Users.Include(u=>u.ItemList)
+                                .FirstOrDefault(u=>u.UserId == HttpContext.Session.GetInt32("UserId"))
+        };
+    return View(MyModels);
+    }
+
+//******************************CREATE ITEM***********************************************
+    [SessionCheck]
+    [HttpPost("items/create")]
+    public ActionResult CreateItem(Item newItem)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Items.Add(newItem);
+            _context.SaveChanges();
+            return RedirectToAction("AddItem");
+        }
+        return View("AddItem");
+    }
+
+//******************************EDIT ITEM FORM***********************************************
+    [SessionCheck]
+    [HttpGet("items/{ItemId}/edit")]
+    public IActionResult EditItem(int ItemId)
+    {
+        MyViewModel MyModels = new MyViewModel
+        {
+            Item = _context.Items.FirstOrDefault(i=>i.ItemId == ItemId),
+            User = _context.Users.Include(u=>u.ItemList)
+                                .FirstOrDefault(u=>u.UserId == HttpContext.Session.GetInt32("UserId"))
+        };
+        return View(MyModels);
+    }
+
+//******************************UPDATE ITEM***********************************************
+    [SessionCheck]
+    [HttpPost("items/{ItemId}/update")]
+    public ActionResult UpdateItem(Item ItemToUpdate, int ItemId)
+    {
+        if (ModelState.IsValid)
+        {
+            Item? OldItem = _context.Items.FirstOrDefault(i=>i.ItemId == ItemId);
+            
+            OldItem.Name = ItemToUpdate.Name;
+            OldItem.Description = ItemToUpdate.Description;
+            OldItem.Image = ItemToUpdate.Image;
+            OldItem.UpdatedAt = DateTime.Now;
+
+            _context.SaveChanges();
+            return RedirectToAction("OneWishList", HttpContext.Session.GetInt32("UserId"));
+        }
+        return View("EditItem", ItemId);
     }
 }
